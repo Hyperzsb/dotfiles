c3a7a6af2300a559f72de1095275becdbbb19387
hyperzsb <hyperzsb@qq.com>
2022-09-27 14:05:44 +0800

    Add update script for .gitconfig and add .gitconfig

---
+++ scripts/git.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+TARGET_FILE=$HOME/.gitconfig
+DESTINATION_FILE=./.gitconfig
+
+if [ -f $TARGET_FILE ] 
+then
+    cp $TARGET_FILE $DESTINATION_FILE
+    echo "$TARGET_FILE updated."
+else
+    echo "$TARGET_FILE not found."
+fi
