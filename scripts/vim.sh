9501173a9acb30550642af0514ff4133f713643b
Shaobo ZHANG <hyperzsb@qq.com>
2024-02-06 10:53:49 +0800

    Update the script for vim
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TARGET_FILE=$HOME/.vimrc
+TARGET_FILE=$HOME/.vim/vimrc
 DESTINATION_FILE=./.vimrc
 
-if [ -f "$TARGET_FILE" ] 
+if [ -f "$TARGET_FILE" ]
 then
     cp "$TARGET_FILE" "$DESTINATION_FILE"
     echo "$TARGET_FILE updated."
