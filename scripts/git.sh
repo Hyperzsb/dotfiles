5dad81cb609d2febcaa2c1301eaafd46e4303c77
Shaobo ZHANG <hyperzsb@qq.com>
2024-03-17 21:28:49 +0800

    Add sensitive data removal logic for the update script for .gitconfig
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,10 +3,21 @@
 TARGET_FILE=$HOME/.gitconfig
 DESTINATION_FILE=./.gitconfig
 
-if [ -f "$TARGET_FILE" ] 
-then
-    cp "$TARGET_FILE" "$DESTINATION_FILE"
-    echo "$TARGET_FILE updated."
-else
-    echo "$TARGET_FILE not found."
+if [[ ! -f "$TARGET_FILE" ]]; then
+    echo "$TARGET_FILE not found. Aborting."
+    exit
 fi
+
+if ! type git > /dev/null; then
+    echo "git not installed. Aborting."
+    exit
+fi
+
+cp "$TARGET_FILE" "$DESTINATION_FILE"
+# Remove sensitive data
+# Sed approach
+sed -i "" "s/signingkey.*$/signingkey = <gpg_key_id>/g" "$DESTINATION_FILE"
+# Vim approach
+#vim -c "%s/signingkey.*$/signingkey = <gpg_key_id>/g" "$DESTINATION_FILE" -c "wq"
+
+echo "$TARGET_FILE updated."
